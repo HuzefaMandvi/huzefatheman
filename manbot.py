113a6e47bafb7edbe8a8bf73fb1b4017a3e120d3
HuzefaMandvi <hzmandviwala@gmail.com>
2018-03-21 21:37:01 +0800

    Fixed author field comparison

--- manbot.py
+++ manbot.py
@@ -1,10 +1,10 @@
 import praw
 
 bot = praw.Reddit(user_agent='Huzefa_TheMan_Bot',
-		  client_id='-XompsHslZP7Gw',
-		  client_secret='rLWt8RLg5QUyHeK42bbi3MIyguk',
-		  username='Huzefa_TheMan_Bot',
-		  password='huzefatheman')
+		client_id='-XompsHslZP7Gw',
+		client_secret='rLWt8RLg5QUyHeK42bbi3MIyguk',
+		username='Huzefa_TheMan_Bot',
+		password='huzefatheman')
 
 subreddit = bot.subreddit('huzefa_theman')
 
@@ -13,6 +13,6 @@ comments = subreddit.stream.comments()
 for comment in comments:
     text = comment.body
     author = comment.author
-    if 'saworetu' in author.lower():
+    if author == 'saworetu':
 	message = "Hello, creator."
 	comment.reply(message)
