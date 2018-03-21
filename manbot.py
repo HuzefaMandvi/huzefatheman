1e4a3f86f8faf32f35080dc83512781355be486e
HuzefaMandvi <hzmandviwala@gmail.com>
2018-03-21 21:28:37 +0800

    Added a simple bot to say hello to me (the overlord/creator)

---
+++ manbot.py
@@ -0,0 +1,18 @@
+import praw
+
+bot = praw.Reddit(user_agent='Huzefa_TheMan_Bot',
+		  client_id='-XompsHslZP7Gw',
+		  client_secret='rLWt8RLg5QUyHeK42bbi3MIyguk',
+		  username='Huzefa_TheMan_Bot',
+		  password='huzefatheman')
+
+subreddit = bot.subreddit('huzefa_theman')
+
+comments = subreddit.stream.comments()
+
+for comment in comments:
+    text = comment.body
+    author = comment.author
+    if 'saworetu' in author.lower():
+	message = "Hello, creator."
+	comment.reply(message)
